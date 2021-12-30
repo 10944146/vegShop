25fc309598c19d3687476f41c9e4c3bbc9d2dd0c
10944146 <92069744+10944146@users.noreply.github.com>
2021-12-30 18:46:21 +0800

    Add files via upload

--- assets/src/cart.js
+++ assets/src/cart.js
@@ -1,38 +1,39 @@
+// 數量/小計計算
+function plus(add){
+    var nex=add.previousElementSibling;
+    nex.value=Number(nex.value)+1;
+    
+    var price = parseInt(add.parentNode.previousElementSibling.innerHTML);
+    var num = price*Number(nex.value);
+    nex.parentNode.nextElementSibling.innerHTML = num;
+}
+function minus(less){
+    var pre =less.nextElementSibling;
+    var price = parseInt(less.parentNode.previousElementSibling.innerHTML);
 
-var add=document.querySelectorAll('.add');  //加號
-var count=document.querySelectorAll('.product-q').value; //件數
-var less=document.querySelectorAll('.less');  //減號
-var price=document.querySelectorAll('.price').value;  //價格
-var amount= document.querySelectorAll('.amount').value; //小計
-// function add(){
-//     count=count+1;
-//     document.querySelectorAll('.product-q').value = count;
-//     document.querySelectorAll('.amount').value = count*price;
-// }
-// count.value.addEventListener('click',add);
-add.onclick = function(){
-    count=count+1;
-    document.querySelectorAll('.product-q').value = count;
-    document.querySelectorAll('.amount').value = count*price;
+    if (pre.value>0) {
+        pre.value=Number(pre.value)-1;
+        var num = price*Number(pre.value);
+        pre.parentNode.nextElementSibling.innerHTML = num;    
+    }else{
+        pre.value=0;
+        var num = price*Number(pre.value);
+        pre.parentNode.nextElementSibling.innerHTML = num;    
+    }    
 }
 
-less.onclick = function(){
-    if (count>0) {
-        count--;
-        document.querySelectorAll('.product-q').value = count;
-        document.querySelectorAll('.amount').value = count*price;
-    } else{
-        count=0;
-        document.querySelectorAll('.product-q').value = count;
-        document.querySelectorAll('.amount').value = count*price;
-    }
-}			
-
-
-
-
+// 刪除按鈕
+function rm(obj){
+    var table=obj.parentNode.parentNode.parentNode; //btn上td上tr上table
+    var tr=obj.parentNode.parentNode;
+    table.removeChild(tr);
+  }
+  
+// 結帳按鈕
 var button1=document.querySelector('#end');
 var total=document.getElementById("total").value;
+// var customer=document.getElementById("customer");
+// var customer=document.getElementById("customer");
 function end(){
     if(total>=1000){
        window.alert("您已下單完成！");
@@ -42,6 +43,3 @@ function end(){
     }
 }
 button1.addEventListener('click',end);
-
-
-
